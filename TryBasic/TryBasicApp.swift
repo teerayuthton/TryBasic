00414edc1e8d46abcd15c7a33a73e01831d0685b
teerayuthjanopas <teerayuth.jan@ascendcorp.com>
2022-05-18 10:22:18 +0700

    Initial Commit

---
+++ TryBasic/TryBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryBasicApp.swift
+//  TryBasic
+//
+//  Created by Ton on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct TryBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
