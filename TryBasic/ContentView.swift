1717ca892860b2ccf72dcff6de1e1fec1ffc7e7d
ToTon <teerayuthton@gmail.com>
2022-05-18 20:52:18 +0700

    Finish Tutorial SwiftUI Fundamentals
    Learning SwiftUI Fundamentals
    From https://cocoacasts.com/swiftui-fundamentals-customizing-views-with-modifiers

--- TryBasic/ContentView.swift
+++ TryBasic/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    //https://cocoacasts.com/swiftui-fundamentals-customizing-views-with-modifiers
+    @State private var showDetails = false
+    
+    let title: String
+    
     var body: some View {
-        Text("Hello, world! Develop")
-            .padding()
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.title)
+                .foregroundColor(.green)
+                .background(.red)
+            Text(title)
+                .underline()
+                .bold()
+                .padding()
+            Button("Show details") {
+                showDetails.toggle()
+            }
+
+            if showDetails {
+                Text("You should follow me on Twitter: @twostraws \n")
+                    .font(.title)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView(title: "previews")
+                    .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
+                .environment(\.sizeCategory, .extraSmall)
+            ContentView(title: "previews")
+                    .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+                .environment(\.sizeCategory, .extraSmall)
+        }
     }
 }
--- TryBasic/TryBasicApp.swift
+++ TryBasic/TryBasicApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TryBasicApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(title: "WindowGroup")
         }
     }
 }
