4fe0d1b83ba649ff32bf85eabec899e9aa37d7d5
ToTon <teerayuthton@gmail.com>
2022-05-20 20:10:08 +0700

    Update menu list
    - Update menu.json for make it menu
    - Update Menu.swift for relate with new Model structure

--- TryBasic/iDine/Menu.swift
+++ TryBasic/iDine/Menu.swift
@@ -17,9 +17,7 @@ struct MenuSection: Codable, Identifiable {
 struct MenuItem: Codable, Equatable, Identifiable {
     var id: UUID
     var name: String
-    var photoCredit: String
-    var price: Int
-    var restrictions: [String]
+    var url: String
     var description: String
 
     var mainImage: String {
@@ -31,6 +29,9 @@ struct MenuItem: Codable, Equatable, Identifiable {
     }
 
     #if DEBUG
-    static let example = MenuItem(id: UUID(), name: "Maple French Toast", photoCredit: "Joseph Gonzalez", price: 6, restrictions: ["G", "V"], description: "Sweet, fluffy, and served piping hot, our French toast is flown in fresh every day from Maple City, Canada, which is where all maple syrup in the world comes from. And if you believe that, we have some land to sell you…")
+    static let example = MenuItem(id: UUID(),
+                                  name: "debug name",
+                                  url: "debug url",
+                                  description: "debug description")
     #endif
 }
